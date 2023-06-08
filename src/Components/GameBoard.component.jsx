b16a4ccdcfb60b10824a5e7ab33abb1cbddbb63f
Dioguit <84785040+Dioguit@users.noreply.github.com>
2023-06-08 19:24:19 +0100

    Update of wining verification

--- src/Components/GameBoard.component.jsx
+++ src/Components/GameBoard.component.jsx
@@ -3,24 +3,88 @@ import './GameBoard.css';
 import SubGame from './SubGame.component';
 
 function GameBoard({ player, onPlayerMove, gameStarted, onGameStart }) {
-    // const [subGameStatus, setSubGameStatus] = useState(false);
+    const [subGameStatus, setSubGameStatus] = useState(false);
     // const [timer, setTimer] = useState();
     // const majorGames = Array(9).fill(0)
     const [majorGames, setMajorGames] = useState(Array(9).fill(0));
 
     useEffect(() => {
         if (gameStarted) {
-            // setSubGameStatus(gameStarted);
+            setSubGameStatus(gameStarted);
             setMajorGames(Array(9).fill(0));
         }
     }, [gameStarted]);
+
+    const OnSubGamedWin = (majorGamesIndex, winner) => {
+        console.log("O vencedor do tabuleiro " + majorGamesIndex + " foi o " + winner);
+        majorGames[majorGamesIndex] = winner;
+
+
+        //Verificar se alguem ganhou e dar update as cores
+        //Verificar Coluna
+        let colVerification = verifyCols(majorGamesIndex);
+        //Verificar Linha
+        let rowVerification =  verifyRows(majorGamesIndex);
+        //Verificar Diagonal
+        let diagVerification = verifyDiag(majorGamesIndex);   
+        
+
+        //ganhar diretamente
+        if(colVerification != 0 || rowVerification != 0 || diagVerification != 0 ){
+            if(majorGames[majorGamesIndex] == 1){
+                console.log("O X Ganhou o tabuleiro Completo")
+            }
+            else{
+                console.log("A O Ganhou o tabuleiro Completo")
+            }
+            
+            return;
+        }      
+        if(majorGames.filter(x => x==0).length === 0){
+           console.log("Acabou em empate")
+        }
+    }
+
+      //Retornar vencedor da coluna, se nao houver, retorna 0
+      function verifyCols(index){
+        if(majorGames[index] == majorGames[(index+3) % 9] && majorGames[(index+3) % 9] == majorGames[(index + 6) % 9]){
+            return(majorGames[index]);
+        } 
+        return 0;
+    }
+
+    function verifyRows(index){
+        let rowNumber = Math.floor(index/3); //numero a dividir por 3 arredondado para baixo
+        let startIndex = rowNumber*3;
+        if(majorGames[startIndex] == majorGames[startIndex+1] && majorGames[startIndex+1] == majorGames[startIndex + 2]){
+            return(majorGames[index]);
+        } 
+        return 0;
+    }
+
+    function verifyDiag(index){
+        if(index % 2 != 0 ){
+            return 0;
+        }
+        if(majorGames[4] == 0){
+            return 0;
+        }
+        if(majorGames[0]==majorGames[4] && majorGames[8]==majorGames[4]){
+            return(majorGames[index])
+        }
+        if(majorGames[2]==majorGames[4] && majorGames[6]==majorGames[4]){
+            return(majorGames[index])
+        }
+        return 0;
+    }
+
 
     return (
         <div className="container board">
             <p>Test Board</p>
             <div className="row">
                 {majorGames.map((item, index) => (
-                    <SubGame key={index} gameStarted={gameStarted} player={player} onPlayerMove={onPlayerMove} />
+                    <SubGame key={index} gameStarted={gameStarted} player={player} onPlayerMove={onPlayerMove} onWinFunc={OnSubGamedWin} majorGamesIndex={index} />
                 ))}
             </div>
         </div>
--- src/Components/SubGame.component.jsx
+++ src/Components/SubGame.component.jsx
@@ -2,54 +2,89 @@ import './SubGame.css';
 import GameCell from './Cell.component';
 import { useState, useEffect } from 'react';
 
-function SubGame({ gameStarted, player, onPlayerMove }) {
 
-    // const minorGames =
+function SubGame({ gameStarted, player, onPlayerMove, onWinFunc,majorGamesIndex }) {
+
     const [minorGames, setMinorGames] = useState(Array(9).fill(0));
     const [colorSwap, setColorSwap] = useState("btn-primary");
-    // function setPlayerCell(index, player) {
-    //     minorGames[index] = player;
-    //     console.log(minorGames); setPlayerCell(index, player);
-    // }
-    
+    const [gameEnd, setGameEnd] = useState(0);
+
     useEffect(() => {
         if (gameStarted) { 
             setMinorGames(Array(9).fill(0));
-            console.log(minorGames);
+            setGameEnd(0);
+            setColorSwap("btn-primary");
         }
     }, [gameStarted]);
-
-    // useEffect(() => {
-    
-    //     if(minorGames.filter(x => x==0).length == 0){
-
-    //     }
-    // }, [minorGames]);
 
     const onCellClick = (index) => {
         console.log("GameStatus" + gameStarted);
         
-        if (!gameStarted || minorGames[index] !== 0) { return; }
+        if (!gameStarted || minorGames[index] !== 0 || gameEnd == 1 ) { return; }
         minorGames[index] = player;
-        console.log(minorGames);
+    
         onPlayerMove()
-
-        // if(minorGames.some(x=>x===0)){
-        // }
-        if(minorGames.filter(x => x==0).length === 0){
-            //swap with wining check
-            if(minorGames.filter(x => x===1).length === minorGames.filter(x => x===-1).length ){
-                setColorSwap("btn-secondary");
-            }
-            else if(minorGames.filter(x => x===1).length > minorGames.filter(x => x===-1).length ){
+        
+        //Verificar se alguem ganhou e dar update as cores
+        //Verificar Coluna
+        let colVerification = verifyCols(minorGames, index);
+        //Verificar Linha
+        let rowVerification =  verifyRows(minorGames, index);
+        //Verificar Diagonal
+        let diagVerification = verifyDiag(minorGames, index);   
+        
+        if(colVerification != 0 || rowVerification != 0 || diagVerification != 0){
+            if(minorGames[index] == 1){
                 setColorSwap("btn-success");
+                onWinFunc(majorGamesIndex, 1);
             }
-            else {
+            else{
                 setColorSwap("btn-danger");
+                onWinFunc(majorGamesIndex, -1);
             }
+            setGameEnd(1);
+            
+            return;
+        }      
+        if(minorGames.filter(x => x==0).length === 0){
+            setColorSwap("btn-secondary");
+            onWinFunc(majorGamesIndex, 0)
+            setGameEnd (1);
         }
     }
 
+    //Retornar vencedor da coluna, se nao houver, retorna 0
+    function verifyCols(minorGames, index){
+        if(minorGames[index] == minorGames[(index+3) % 9] && minorGames[(index+3) % 9] == minorGames[(index + 6) % 9]){
+            return(minorGames[index]);
+        } 
+        return 0;
+    }
+
+    function verifyRows(minorGames, index){
+        let rowNumber = Math.floor(index/3); //numero a dividir por 3 arredondado para baixo
+        let startIndex = rowNumber*3;
+        if(minorGames[startIndex] == minorGames[startIndex+1] && minorGames[startIndex+1] == minorGames[startIndex + 2]){
+            return(minorGames[index]);
+        } 
+        return 0;
+    }
+
+    function verifyDiag(minorGames, index){
+        if(index % 2 != 0 ){
+            return 0;
+        }
+        if(minorGames[4] == 0){
+            return 0;
+        }
+        if(minorGames[0]==minorGames[4] && minorGames[8]==minorGames[4]){
+            return(minorGames[index])
+        }
+        if(minorGames[2]==minorGames[4] && minorGames[6]==minorGames[4]){
+            return(minorGames[index])
+        }
+        return 0;
+    }
 
     return (
         <div className='col-md-4'>
